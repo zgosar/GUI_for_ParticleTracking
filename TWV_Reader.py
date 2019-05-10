0b284197cf0d63eda1f5dd7e0284c78540cdb52d
zgosar <gosar.ziga@gmail.com>
2019-05-10 11:35:41 +0200

    Improved gui-less version. But for guiless look to the common repo for the official version.

--- TWV_Reader.py
+++ TWV_Reader.py
@@ -16,7 +16,7 @@ import unittest
 from pims import FramesSequence, Frame
 import numpy as np
 
-VERBOSE_LEVEL = 2
+VERBOSE_LEVEL = 0
 
 class TArframe_ROI(Structure):
     _pack_ = 1
@@ -69,12 +69,12 @@ class TArframe_header(Structure):
                 ("CalibrationData1", TArTrapData),
                 ("CalibrationData2", TArTrapData),
                 ("CalibrationData3", TArTrapData),
-                ("CalibrationData4", TArTrapData)         
+                ("CalibrationData4", TArTrapData)
                 ]
 
 def display_attr(obj):
     """
-    Prints obj.
+    Prints obj. Used for testing.
     """
     for field_name, field_type in obj._fields_:
         print(field_name, getattr(obj, field_name))
@@ -83,8 +83,14 @@ class TWV_Reader(FramesSequence):
     """
     Reader for .twv files for pims package.
     """
-    
+
     def __init__(self, filename):
+        """
+        Inputs:
+         - filename: Filename string, ending with .twv.
+
+        Reads video header and prepares video header data.
+        """
         self.filename = filename
         self.f = open(filename, "rb")
 
@@ -94,7 +100,7 @@ class TWV_Reader(FramesSequence):
         if self.video_header.FrameData.BytesPerPixel != 1:
             from warnings import warn
             warn("video_header.FrameData.BytesPerPixel is not 1")
-       
+
         self._len = self.video_header.RecordedFrames
         self._dtype = np.uint8
         self._frame_shape = (self.video_header.FrameData.ROI.Width,
@@ -160,22 +166,9 @@ class TWV_Reader(FramesSequence):
 
         return Frame(image, frame_no=frame_no)
 
-    def get_frame_time_old(self, frame_no):
-        """
-        OBSOLETE.
-        Use get_frame_time.
-        """
-        frame_header_size = self.video_header.FrameData.HeaderSize
-        self.f.seek(self.video_header.VideoHeaderSize)
-        self.f.seek(frame_no * (self._frame_shape[0] * self._frame_shape[1] + frame_header_size),
-                    1)
-        frame_header = TArframe_header()
-        self.f.readinto(frame_header)
-        return frame_header.FrameTime
-
     def get_frame_time(self, frame_no):
         """
-        Returns the timestamo of a frame.
+        Returns the timestamp of a frame.
         """
         self.f.seek(self.video_header.VideoHeaderSize) # absolute seek
         self.f.seek(frame_no * self.frame_size_bytes, 1) # relative seek
@@ -187,31 +180,44 @@ class TWV_Reader(FramesSequence):
         return self._len
 
     def __del__(self):
+        """
+        Closes the file on object destruction.
+        Also seems to close the file when program crashes.
+        """
         self.f.close()
 
     @property
     def frame_shape(self):
+        """
+        Mandaroty for pims.
+        """
         return self._frame_shape
 
     @property
     def pixel_type(self):
+        """
+        Mandaroty for pims.
+        """        
         return self._dtype
 
     @classmethod
     def class_exts(cls):
+        """
+        Mandaroty for pims.
+        """
         return {'twv'} | super(TWV_Reader, cls).class_exts()
 
     def check_for_time_jumps(self, treshold=10**-2, show=False):
         """
         Checks if there are any unusual time jumps in the file.
-        Returns True if data is OK (max_time-min_time)/min_time <= treshold.
+        Returns True if data is OK max_time > frame_time * (1 + treshold) or
+            min_time < frame_time * (1 - treshold)
         Kwargs:
          - treshold=10**-2: If relative error is below this treshold,
              video is considered OK.
          - show=False: If true, the time vs. frame number and time step are plotted
              (matplotlib required).
 
-        TODO: Compare to reported FPS, instead of with each other.
         """
         times = []
         self.f.seek(self.video_header.VideoHeaderSize) # absolute seek
@@ -220,24 +226,24 @@ class TWV_Reader(FramesSequence):
         for _ in range(self._len):
             frame_header = TArframe_header()
             self.f.readinto(frame_header)
-            
+
             times.append(frame_header.FrameTime)
             self.f.seek(self._frame_shape[0] * self._frame_shape[1], 1) # relative seek
-            
+
         times = np.array(times)
         time_deltas = times[1:]-times[:-1]
         min_time = min(time_deltas)
         max_time = max(time_deltas)
         if VERBOSE_LEVEL >= 2:
             print("Min frame time {:}, max frame time {:}, frame_time {:}".format(
-                min_time, max_time, frame_time))
+                  min_time, max_time, frame_time))
         ret = True
         if (max_time > frame_time * (1 + treshold) or
             min_time < frame_time * (1 - treshold)):
             if VERBOSE_LEVEL >= 1:
                 print("There are time jumps: min_time {:}, max_time {:},"
-                    "relative difference {:}, treshold {:}".format(
-                      min_time, max_time, (max_time-min_time)/min_time, treshold))
+                      "relative difference {:}, treshold {:}".format(
+                        min_time, max_time, (max_time-min_time)/min_time, treshold))
             ret = False
         if show:
             import matplotlib.pyplot as plt
@@ -247,7 +253,8 @@ class TWV_Reader(FramesSequence):
             ax[0].set_ylabel("Time from start [s]")
             
             ax[1].plot(range(len(times)-1), time_deltas, label="Actual time between frames")
-            ax[1].plot([0, len(times)-1], [frame_time, frame_time], label="Reported time between frames")
+            ax[1].plot([0, len(times)-1], [frame_time, frame_time],
+                       label="Reported time between frames")
             ax[1].legend()
             ax[1].semilogy()
             ax[0].set_title("avg FPS = {:}, reported FPS = {:}".format(
@@ -269,7 +276,6 @@ class TWV_Reader(FramesSequence):
         If fname is set, it is written to it (tab separated values).
         Otherwise it is returned as an array.
         """
-        
         if VERBOSE_LEVEL > 1:
             print("get_all_tweezer_positions")
         laser_powers = [] # TODO
@@ -289,7 +295,7 @@ class TWV_Reader(FramesSequence):
                     tmp.PositionY,
                     tmp.Intensity])
             self.f.seek(self._frame_shape[0] * self._frame_shape[1], 1) # relative seek
-            
+
         times = np.array(times)
         if fname is None:
             return times, laser_powers, traps
@@ -299,44 +305,56 @@ class TWV_Reader(FramesSequence):
             # TODO
             # this will not be used. This will be used together
             # with particle tracker.
-            
+
 class Test(unittest.TestCase):
+    """
+    Additional tests of this module happen in particle tracker module.
+    """
 
     def setUp(self):
         #self.twv_reader = TWV_Reader("passiveInTrapP1.twv")
-        pass
-    
+        self.filename = "tests/test_example.twv"
+
     def test_set_end_frame_number(self):
-        twv_reader = TWV_Reader("passiveInTrapP1.twv")
+        twv_reader = TWV_Reader(self.filename)
         twv_reader.set_end_frame(50)
         self.assertEqual(len(twv_reader), 50)
 
     @unittest.expectedFailure
     def test_check_end_frame_too_high(self):
-        twv_reader = TWV_Reader("passiveInTrapP1.twv")
+        twv_reader = TWV_Reader(self.filename)
         twv_reader.set_end_frame(100000)
         twv_reader.f.close()
-        
 
-if __name__ == '__main__' and 1:
-    # TODO select examle videos for test
-    filename = "passiveInTrapP1.twv"
-    #filename = "F:/Gersak/190226_Voda23_2TrapsAS_20kHz.twv"
-    #filename = "F:/DefaultVideo_15.twv"
-    #print(get_all_frame_times(filename, show=True, treshold=10**-2))
-    a = TWV_Reader(filename)
-    print('filename', a.filename)
-    a.set_end_frame(10000)
-    c = a.get_all_metadata()
-    a.check_for_time_jumps(show=True)
-    #times, laser_powers, traps = a.get_all_tweezer_positions()
+    def test_check_metadata(self):
+        twv_reader = TWV_Reader(self.filename)
+        calculated = twv_reader.get_all_metadata()
+        expected = {'ROI': {'Width': 416, 'Left': 960, 'Height': 272, 'Top': 840},
+                    'Exposure': 1.9845360000000003, 'FrameRate': 100.0,
+                    'ImageToSampleScale': 10.666666984558105, 'Gain': 1.0}
+        self.assertEqual(expected, calculated)
+
+def example(filename):
+    """
+    Example usega of TWV_Reader.
+    """
+    frames = TWV_Reader(filename)
+
+    metadata = frames.get_all_metadata()
+
+    frames.check_for_time_jumps(show=False)
+
+    times, laser_powers, traps = frames.get_all_tweezer_positions()
+
     #import cv2
     #cv2.namedWindow('image',cv2.WINDOW_NORMAL)
     #for i in range(a.video_header.RecordedFrames):
     #    cv2.imshow('image', a[i])
     #    cv2.waitKey(1)
 
+if __name__ == '__main__' and 1:
+    example("tests/test_example.twv")
 
 if __name__ == '__main__':
     unittest.main()
-    
+
--- guiless_example.py
+++ guiless_example.py
@@ -1,19 +1,35 @@
+"""
+Tracks particles.
+Opens file, tracks particles, links particles, gets trap data and saves
+everything into .dat file.
+"""
+
+import unittest
 import pims
 import trackpy as tp
 import TWV_Reader
 
 def save_everything(filename, frames, trajectories, times, laser_powers, traps):
     """
-    Converts all data to .dat file.
+    Converts all data to a (.dat) file.
     Important NOTE:
     The format will change in the future. Particle data and trap data will go into separate files,
     and additional trap metadata will be saved.
-    Current format:
+    
+    Inputs:
+     - filename: Output filename.
+     - frames: TWV_Reader or other pims object. __len__ needs to be defined.
+     - trajectories: Pandas DataFrame with all the particles. Same structure as returned by TrackPy.
+     - times: List of frame times.
+     - laser powers: List of laser powers for every frame.
+     - traps: List of traps data for every frame. traps[frame number][trap number][power/position_x/position_y]
+     
+    Output format, tab separated:
      - time, laser power, trap_1_power, trap_1_x, trap_1, y,
-     the same for traps 2-4, particle_1_x, particle_1_y,
-     the same for all particles
-    if a particle is missing from a frame, empty string '' is placed
-    instead of coordinates. 
+         the same for traps 2-4, particle_1_x, particle_1_y,
+         the same for all particles
+    If a particle is missing from a frame, empty string ('') is placed
+    instead of coordinates.
     """
     max_particles = int(round(trajectories.max()['particle']))
     with open(filename, 'w') as f:
@@ -21,37 +37,79 @@ def save_everything(filename, frames, trajectories, times, laser_powers, traps):
             tmp = ''
             tmp += str(times[i]) + '\t'
             tmp += str(laser_powers[i]) + '\t'
-            for j in range(4):
-                for k in range(3):
+            for j in range(4): # for j in traps
+                for k in range(3): # for k in power/x/y of a trap
                     tmp += str(traps[j][i][k]) + '\t'
             for j in range(max_particles+1):
                 tmp_particle = trajectories.loc[
                     trajectories['particle'] == j].loc[
                         trajectories['frame'] == i]
+                # find the particle j on frame i
                 if tmp_particle.empty:
                     tmp += '\t\t'
+                    # if no such particle exists, write two tabs
                 else:
                     tmp += str(tmp_particle.iloc[0]['x']) + '\t'
                     tmp += str(tmp_particle.iloc[0]['y']) + '\t'
+                    # else write the particles position
             tmp += '\n'
             f.write(tmp)
 
-if __name__ == '__main__':
+class Test(unittest.TestCase):
+    """
+    Also tests TWV_Reader module.
+    """
 
-    FILENAME = 'passiveInTrapP1.twv'
+    def setUp(self):
+        pass
 
-    frames = pims.open(FILENAME)
-    frames.set_end_frame(50)
+    def test_everything(self):
+        """
+        Test the whole tracking pipeline from input file to particle and trap
+        positions in the output file.
+        """
+        filename = "tests/test_example.twv"
+        frames = pims.open(filename)
+        times, laser_powers, traps = frames.get_all_tweezer_positions()
+        features = tp.batch(frames, 25, minmass=1000, invert=False)
+        tracks = tp.link_df(features, 15, memory=10)
+        save_everything(filename[:-4] + '_out.dat', frames, tracks, times, laser_powers, traps)
+        with open(filename[:-4] + '_out.dat', 'r') as calculated_file:
+            with open(filename[:-4] + '_expected.dat', 'r') as expected_file:
+                for calculated, expected in zip(calculated_file, expected_file):
+                    self.assertEqual(calculated, expected)
 
-    metadata = frames.get_all_metadata()
+
+def example(filename):
+    """
+    Example usecase from input file to particle and trap positions in .dat file.
+    """
+    frames = pims.open(filename)
+    # Open file with pims. Works with many file extensions.
+    # This example assumes .twv file.
+
+    # metadata = frames.get_all_metadata()
+    # Optional access to additional metadata.
 
     times, laser_powers, traps = frames.get_all_tweezer_positions()
+    # Obtain frame times, laser power at each frame time and
+    # traps powers and positions at each frame.
 
-    f = tp.batch(frames, 25, minmass=1000, invert=False)
-    # Diameter is 25. Must be odd number.
-    # The parameters for batch are recommended to be determined using GUI.
+    features = tp.batch(frames, 25, minmass=1000, invert=False)
+    # Obtain features (particle positions) using trackpy's batch function.
+    # It is verbose.
+    # The 25 in arguments is diameter. It be odd number.
+    # It is recommended to obtain parameters using GUI.
+    
+    tracks = tp.link_df(features, 15, memory=10)
+    # Joins particles positions to tracks (connects them in time).
+    # See trackpy documentation for parameters.
 
-    t = tp.link_df(f, 15, memory=10)
-    # See trackpy documentation for above parameters.
+    save_everything(filename[:-4] + '_out.dat', frames, tracks, times, laser_powers, traps)
+    # Save data in a format readable by other scripts.
 
-    save_everything(FILENAME[:-4] + '_out.dat', frames, t, times, laser_powers, traps)
+if __name__ == "__main__" and 1:
+    example("tests/test_example.twv")
+
+if __name__ == "__main__":
+    unittest.main()
